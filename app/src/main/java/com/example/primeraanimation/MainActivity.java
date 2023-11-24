2993b96561775de24c2f41995c0b2e93689fc0f5
yaracsoto88 <_cedenosoto@cifpfbmoll.eu>
2023-11-24 18:01:41 +0100

    Initial commit

---
+++ app/src/main/java/com/example/primeraanimation/MainActivity.java
@@ -0,0 +1,90 @@
+package com.example.primeraanimation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LayoutAnimationController;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        startAnimation();
+        Button btnCambiarActividad = findViewById(R.id.button);
+        btnCambiarActividad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cambiarActividad();
+            }
+        });
+    }
+
+
+    private void startAnimation() {
+        TextView logo1 = (TextView) findViewById(R.id.TextViewTopTitle);
+        Animation fade1 = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        logo1.startAnimation(fade1);
+        Animation spinin = AnimationUtils.loadAnimation(this, R.anim.custom_anim);
+        LayoutAnimationController controller =
+                new LayoutAnimationController(spinin);
+        TableLayout table = (TableLayout) findViewById(R.id.TableLayout01);
+        for (int i = 0; i < table.getChildCount(); i++) {
+            TableRow row = (TableRow) table.getChildAt(i);
+            row.setLayoutAnimation(controller);
+        }
+    }
+
+    private void cambiarActividad() {
+
+        TextView logo1 = findViewById(R.id.TextViewTopTitle);
+
+        Animation fade2 = AnimationUtils.loadAnimation(this, R.anim.fade_in2);
+
+        fade2.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                startActivity(new Intent(MainActivity.this, QuizMenuActivity.class));
+                finish();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        logo1.startAnimation(fade2);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Stop the animation
+        TextView logo1 = (TextView) findViewById(R.id.TextViewTopTitle);
+        logo1.clearAnimation();
+        TextView logo2 = (TextView) findViewById(R.id.TextViewBottomTitle);
+        logo2.clearAnimation();
+        // ... stop other animations
+        TableLayout table = (TableLayout) findViewById(R.id.TableLayout01);
+        for (int i = 0; i < table.getChildCount(); i++) {
+            TableRow row = (TableRow) table.getChildAt(i);
+            row.clearAnimation();
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/primeraanimation/QuizMenuActivity.java
@@ -0,0 +1,18 @@
+package com.example.primeraanimation;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class QuizMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+}
